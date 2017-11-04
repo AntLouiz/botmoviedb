3b8f7158728782aa980c0d73638e3e9ab5d4b36f
Luiz <luizrodrigo46@hotmail.com>
2017-11-03 22:43:53 -0300

    the first implementation

--- mybot.py
+++ mybot.py
@@ -1,67 +1,138 @@
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from emoji import emojize
+from telegram.ext import (
+  Updater,
+  RegexHandler,
+  CommandHandler,
+  MessageHandler,
+  Filters
+)
 from decouple import config
 import tmdbsimple as tmdb
+
+
+def start(bot, update):
+  """
+    Show a welcome message
+  """
+
+  msg = "Hi, send a message like \"*find* Thor: Ragnarok\" to see the movie overview."
+  bot.send_message(
+    chat_id=update.message.chat_id,
+    text=msg,
+    parse_mode=telegram.ParseMode.MARKDOWN
+  )
+
+def get_movie(bot, update, groups):
+  """
+    Shows the movie overview
+  """
+
+  def send_movie_message(matched_movie, bot, chat_id):
+    title = matched_movie['title']
+    overview = matched_movie['overview']
+
+    # print("Title: {}\nOverview: \n{}".format(title, overview))
+    bot.send_message(chat_id=chat_id, text=title)
+
+    if(matched_movie['poster_path']):
+      image_link = image_url+matched_movie['poster_path']
+      # print("Image Link: {}".format(image_link))
+      bot.send_photo(chat_id=chat_id, photo=image_link)
+
+    bot.send_message(chat_id=chat_id, text=overview)
+    # print("\n")
+    pass
+
+  def send_movies_message_keyboard(matched_movies, bot, chat_id):
+    kb = []
+    msg = "I found that movies, check if your movie is in the keyboard options."
+
+    for movie in matched_movies:
+      title = movie['title']
+      kb.append([telegram.KeyboardButton("find {}".format(title))])
+
+    kb_markup = telegram.ReplyKeyboardMarkup(kb)
+    bot.send_message(
+      chat_id=chat_id,
+      text=msg,
+      reply_markup=kb_markup
+    )
+    pass
+
+
+  movie_name = groups[0]
+  chat_id = update.message.chat_id
+
+  if not movie_name:
+    bot.send_message(
+      text="Please insert a movie name after \"*find*\".",
+      parse_mode=telegram.ParseMode.MARKDOWN
+    )
+    return True
+
+  tmdb.API_KEY = config('MOVIE_DB_TOKEN')
+  search = tmdb.Search()
+
+  image_url = "https://image.tmdb.org/t/p/w185"
+  response = search.movie(query=movie_name)
+  results = search.results
+  num_results = len(results)
+
+  if num_results > 1:
+    movies = results
+    send_movies_message_keyboard(movies, bot, chat_id)
+
+  elif num_results == 1:
+    movie = results[0]
+    send_movie_message(movie, bot, chat_id)
+
+  else:
+    msg = "Movie not found, please try again."
+    bot.send_message(chat_id=chat_id, text=msg)
+
+def support(bot, update):
+  """
+    Shows a help message.
+  """
+
+  msg = "Send \"*find* <my_movie>\" to get the film overview."
+
+  bot.send_message(
+    chat_id= update.message.chat_id,
+    text=msg,
+    parse_mode=telegram.ParseMode.MARKDOWN
+  )
+
+def default(bot, update):
+  """
+    A default message to unknown command messages.
+  """
+
+  msg = "Sorry, i don\'t understanding that command. Send [/help] to show a help message."
+  msg += emojize(':pensive:', use_aliases= True)
+
+  bot.send_message(
+    chat_id=update.message.chat_id,
+    text=msg,
+    parse_mode=telegram.ParseMode.MARKDOWN
+  )
+
+start_handler = CommandHandler('start', start)
+help_handler = CommandHandler('support', support)
+
+get_movie_handler = RegexHandler(r"find\s+(.*)$", get_movie, pass_groups=True)
+default_handler = MessageHandler(Filters.command, default)
+
 
 
 updater = Updater(token=config('TELEGRAM_BOT_TOKEN'))
 dispatcher = updater.dispatcher
 
-
-def start(bot, update):
-  """
-    Mostra a mensagem de bem-vindo
-  """
-
-  msg = "Hello, send /movie to see the movie overview."
-  bot.send_message(
-    chat_id=update.message.chat_id,
-    text=msg
-  )
-
-def default(bot, update):
-  '''
-    A default message to unknown users messages.
-  '''
-
-  msg = 'Sorry, i don\'t understanding that command '
-  msg += emojize(':pensive:', use_aliases= True)
-
-  bot.send_message(
-    chat_id= update.message.chat_id,
-    text= msg
-  )
-
-default_handler = MessageHandler(Filters.command, default)
-start_handler = CommandHandler('start', start)
-
-dispatcher.add_handler(default_handler)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(help_handler)
+dispatcher.add_handler(default_handler)
+dispatcher.add_handler(get_movie_handler)
+
 
 updater.start_polling()
-
-"""tmdb.API_KEY = config('MOVIE_DB_TOKEN')
-# bot token 462659520:AAH8oMaK3NS2wvfBWPYLMUzvdt_TKlH-_Uc
-
-
-search = tmdb.Search()
-
-# movie_name = input("Insira o nome de um filme: ")
-movie_name = "Batman vs Superman"
-
-image_url = "https://image.tmdb.org/t/p/w185"
-# bot.send_photo(chat_id=chat_id, photo='https://telegram.org/img/t_logo.png')
-
-response = search.movie(query=movie_name)
-
-for s in search.results:
-  title = s['title']
-  overview = s['overview']
-
-  print("Title: {}\nOverview: \n{}".format(title, overview))
-
-  if(s['poster_path']):
-    image_link = image_url+s['poster_path']
-    print("Image Link: {}".format(image_link))
-
-  print("\n")"""
