875a901383e47703971e29b4a8154fab83f40091
AntLouiz <luizrodrigo46@hotmail.com>
2018-06-29 20:16:20 -0300

    logger added

--- settings.py
+++ settings.py
@@ -1,4 +1,12 @@
+import logging
 from decouple import config
 
 TELEGRAM_BOT_TOKEN = config('TELEGRAM_BOT_TOKEN')
 MOVIE_DB_TOKEN = config('MOVIE_DB_TOKEN')
+
+logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.DEBUG)
