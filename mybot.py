e8fa86b7a8b16846a7f742e3d53ce18571c21e14
AntLouiz <luizrodrigo46@hotmail.com>
2018-08-25 13:31:38 -0300

    main function added to the main module

--- mybot.py
+++ mybot.py
@@ -8,13 +8,15 @@ from handlers import (
 from settings import TELEGRAM_BOT_TOKEN
 
 
-updater = Updater(token=TELEGRAM_BOT_TOKEN)
-dispatcher = updater.dispatcher
+def main():
+    updater = Updater(token=TELEGRAM_BOT_TOKEN)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(default_handler)
+    dispatcher.add_handler(get_movie_handler)
 
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(help_handler)
-dispatcher.add_handler(default_handler)
-dispatcher.add_handler(get_movie_handler)
+    updater.start_polling()
 
-
-updater.start_polling()
+if __name__ == '__main__':
+    main()
